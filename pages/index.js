006f95f5abf2a7139dc526620c6d51e60d7f653c
suhdanny <suhdong21@gmail.com>
2022-07-24 21:04:33 -0400

    Install tailwind

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,5 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-	return <div className={styles.container}></div>;
+	return <h1 className='text-red-500'>Home</h1>;
 }
