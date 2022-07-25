422f90fc9b988c975f8f43ab07bf2a6d29eb38b4
suhdanny <suhdong21@gmail.com>
2022-07-25 00:25:43 -0400

    Add location and destination input components

---
+++ pages/components/Action.js
@@ -0,0 +1,58 @@
+import tw from 'tailwind-styled-components';
+import { useState } from 'react';
+
+const Action = () => {
+	const [locationInputFocus, setLocationInputFocus] = useState(false);
+	const [destinationInputFocus, setDestinationInputFocus] = useState(false);
+
+	const headingElement = () => {
+		if (locationInputFocus) {
+			return <Heading>Where to pick you up?</Heading>;
+		} else if (destinationInputFocus) {
+			return <Heading>Where to drop you off?</Heading>;
+		}
+		return <Heading>Where would you like to go?</Heading>;
+	};
+
+	return (
+		<Wrapper>
+			{headingElement()}
+			<InputContainer>
+				<Input placeholder='Enter your location' onFocus={() => setLocationInputFocus(true)} onBlur={() => setLocationInputFocus(false)} />
+				<Input
+					placeholder='Enter your destination'
+					onFocus={() => setDestinationInputFocus(true)}
+					onBlur={() => setLocationInputFocus(false)}
+				/>
+				<Button>Confirm Locations</Button>
+			</InputContainer>
+			<RideContainer></RideContainer>
+		</Wrapper>
+	);
+};
+
+const Wrapper = tw.div`
+    absolute bg-white z-10 h-[80vh] w-[30vw] rounded left-[5%] top-[12%] p-5 flex flex-col
+`;
+
+const Heading = tw.div`
+    text-2xl text-black font-bold mb-3
+`;
+
+const InputContainer = tw.div`
+    flex flex-col gap-3
+`;
+
+const Input = tw.input`
+    h-12 bg-gray-100 focus:outline-none focus:border-2 focus:border-black pl-3
+`;
+
+const Button = tw.button`
+    h-10 bg-black text-white
+`;
+
+const RideContainer = tw.div`
+    flex-1 mt-3 bg-gray-100
+`;
+
+export default Action;
--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,18 @@ import { useEffect } from 'react';
 import tw from 'tailwind-styled-components';
 import Header from './components/Header';
 import Map from './components/Map';
+import Action from './components/Action';
 
 export default function Home() {
 	return (
 		<Wrapper>
 			<Header />
 			<Map />
+			<Action />
 		</Wrapper>
 	);
 }
 
 const Wrapper = tw.div`
-	flex flex-col h-screen
+	flex flex-col h-screen relative
 `;
